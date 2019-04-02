c393774681e2b221742ba715dfa43629d1ca7203
Katharine Cooney <katharinecooney@gmail.com>
2019-04-03 00:00:04 +0200

    Add solutions for Names and Inputs section

--- basic-algorithms.js
+++ basic-algorithms.js
@@ -1,6 +1,12 @@
 // Names and Input
 
+var driver = "Sam";
 
+console.log("The driver's name is " + driver);
+
+var hacker2 = prompt("What's your name?");
+
+console.log("The navigator's name is " + hacker2);
 //Conditionals
 
 
